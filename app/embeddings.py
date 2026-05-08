0c5cca7d6472f5cac8841c1df823936713692e1c
Mohan Selvan <mohan70330@gmail.com>
2026-05-09 00:56:48 +0530

    project reset

--- app/embeddings.py
+++
@@ -1,8 +0,0 @@
-import os
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-
-def get_gemini_embeddings():
-    return GoogleGenerativeAIEmbeddings(
-        model="models/embedding-001",
-        google_api_key=os.getenv("GOOGLE_API_KEY")
-    )
